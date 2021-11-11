34a58a1a8332c1875fdef41d0e670ad20d533d34
darthcody91 <thomascody91@gmail.com>
2021-11-11 08:41:45 -0700

    cssstorm

---
+++ lesson8-2/js/dates.js
@@ -0,0 +1,5 @@
+
+document.getElementById("copyrightyear").innerHTML = new Date().getFullYear();
+
+document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"}) 
+
