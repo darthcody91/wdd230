d79b2721facfbeaa8687b2c723482950ee424b1a
darthcody91 <thomascody91@gmail.com>
2021-11-24 18:09:19 -0700

    lesson10

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -5,32 +5,39 @@ fetch(forecastURL)
   .then((response) => response.json())
   .then(data => {
  
+  
   const noon = data.list.filter(x => x.dt_txt.includes('12:00:00'));
   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const d = new Date(data.list[0].dt* 1000);
-    const dayName = days[d.getDay()];
-    console.log(dayName)
- 
+  const d = new Date(data.list[0] * 1000);
+  const weekday = days[d.getDay()]; //this is where the days from the json goes --- figure out how to loop the date//
+    console.log(weekday)
+  
+
+    
+
   
     noon.forEach(x => {
     let card = document.createElement('section') 
     let day = document.createElement('h3');
-    let dayName = document.createElement('h3');
     let temp = document.createElement('p');
     let desc = document.createElement('p');
+  
     
 
     day.innerHTML = `${x.dt_txt}`;
     temp.innerHTML = `${x.main.temp} °F`;
     desc.innerHTML = `${x.weather[0].description}`.toUpperCase(); 
-    
+      
+
    
 
+ 
+    
 
     card.appendChild(day);
-    card.appendChild(dayName);
     card.appendChild(temp);
     card.appendChild(desc);
+   
    
 
     document.querySelector('div.cards').appendChild(card);
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -7,6 +7,14 @@ fetch(apiURL)
     document.getElementById('wind-speed').textContent = jsObject.wind.speed;
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('current').textContent = jsObject.weather[0].main;
+
+var temp= 'current-temp';
+var wSpeed= 'wind-speed';
+var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+
+var windChill= Math.round(windChill);
+document.getElementById("wind-chill").innerHTML= windChill;
+
 });
 
 
