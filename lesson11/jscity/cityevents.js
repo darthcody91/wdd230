2d2966f15ad65f3e9e025f465a506f60bba34952
darthcody91 <thomascody91@gmail.com>
2021-11-29 00:03:27 -0700

    cityeventupdate

---
+++ lesson11/jscity/cityevents.js
@@ -0,0 +1,26 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); 
+        
+    const towns = jsonObject['towns'];  
+    //get city data for events//
+    //change name of city on each JS page//
+    const cityname = towns.filter((towns) => towns.name == "Preston");
+    let cityevents = cityname[0].events;
+    let head = document.createElement('h2'); 
+
+
+    //loops events//
+    cityevents.forEach(event => {
+    let list = document.createElement('p');
+    list.innerHTML = event;
+    head.appendChild(list);   
+     }); 
+    
+    document.getElementById("eventdata").appendChild(head);
+});
+
--- lesson11/jscity/dates.js
+++ lesson11/jscity/dates.js
@@ -1,5 +1,4 @@
 
 document.getElementById("copyrightyear").innerHTML = new Date().getFullYear();
 
-document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"}) 
-
+document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"})
