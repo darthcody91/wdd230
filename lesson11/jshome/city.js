31a03c91be2a1cd3d3721608e28fb49413ce0263
darthcody91 <thomascody91@gmail.com>
2021-11-28 20:54:19 -0700

    final_1

---
+++ lesson11/jshome/city.js
@@ -0,0 +1,41 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); 
+        
+        const towns = jsonObject['towns'];
+
+        const towndata = towns.filter((towns) => towns.name === 'Soda Springs' || towns.name === 'Preston' || towns.name === 'Fish Haven');
+        towndata.forEach((x) => {
+        let card = document.createElement('section');
+        let name= document.createElement('h2');
+        let motto = document.createElement('h3');
+        let yearfounded = document.createElement('p');
+        let population = document.createElement('p');
+        let rainfall = document.createElement('p');
+        let imagesrc = "lesson11/imageshome" + x.photo[0]+ '.png';
+        let image = document.createElement('img');
+
+        name.innerHTML = `${x.name}`;
+        motto.innerHTML = `${x.motto}`;
+        yearfounded.innerHTML = `Year Founded - ${x.yearFounded}`;
+        population.innerHTML = `Population - ${x.currentPopulation}`;
+        rainfall.innerHTML = `Annual Rain Fall - ${x.averageRainfall}`;
+        image.setAttribute('src', imagesrc);
+
+
+        card.appendChild(name);
+        card.appendChild(motto);
+        card.appendChild(yearfounded);
+        card.appendChild(population);
+        card.appendChild(rainfall);
+        card.appendChild(image);
+
+        document.querySelector('div.cards').appendChild(card);
+
+        });
+    });
+
